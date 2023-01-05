84944c2bf7ce5a29aa647d9ce15189d4af77de56
braquelw <appaloosarider7@gmail.com>
2023-01-05 15:14:04 -0700

    updated java for the date

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,13 @@ const copyright = document.querySelector('#copyright');
 const lastModified = document.querySelector('#lastModified');
 
 const currentYear = new Date().getFullYear();
-copyright.textContent = '©️ ${currentYear}';
+copyright.textContent = `©️ ${currentYear}`;
 
-let olastModif = new Date(document.lastModified);
-lastModified.textContent = '${olastModif}'
+let lastModif = new Date(document.lastModified);
+let month = lastModif.getMonth()+1;
+let day = lastModif.getDate();
+let year = lastModif.getFullYear();
+let hours = lastModif.getHours();
+let mins = lastModif.getMinutes();
+let secs = lastModif.getSeconds();
+lastModified.textContent = `${month}/${day}/${year} ${hours}:${mins}:${secs}`
