80dca0cc4ca721cbb5ecc809583f15c30a17482b
braquelw <appaloosarider7@gmail.com>
2023-02-03 15:40:58 -0700

    improved home page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -45,3 +45,6 @@ var dayofweek = weekday[currentDate.getDay()];
 if (dayofweek == weekday[1] || dayofweek == weekday [2]) {
     banner.textContent = `🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.`
 }
+else {
+    banner.style.display = 'none';
+}
