d7514d0b298790f558d8e95bf147e2224df185b9
braquelw <appaloosarider7@gmail.com>
2023-03-01 17:45:42 -0700

    added directory page

--- chamber/scripts/gridlist.js
+++ chamber/scripts/gridlist.js
@@ -1,20 +1,40 @@
-const url = "";
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
+
+
+
+const url = "json/data.json";
 
 // fetch data from json
 async function getBusinessData() {
     const response = await fetch(url);
     const data = await response.json();
-    //console.table(data.prophets);
     displayBusinesses(data.businessDirectory);
 }
 
-getBusinessData();
+getBusinessData()
 
-// display prophets
+// display businesses
 const displayBusinesses = (businessDirectory) => {
     const cards = document.querySelector('section.cards');
 
-    prophets.forEach((prophet) => {
+    businessDirectory.forEach((business) => {
         let card = document.createElement('section');
         let name = document.createElement('h3');
         let address = document.createElement('p');
@@ -22,25 +42,25 @@ const displayBusinesses = (businessDirectory) => {
         let website = document.createElement('a')
         let image = document.createElement('img');
 
-        name.textContent = `${businessDirectory.name}`;
+        name.textContent = `${business.name}`;
 
-        address.textContent = `${businessDirectory.address}`;
-        phone.textContent = `${businessDirectory.phone}`;
+        address.textContent = `${business.address}`;
+        phone.textContent = `${business.phone}`;
 
-        website.setAttribute('href', `${businessDirectory.website}`);
+        website.setAttribute('href', `${business.website}`);
         website.textContent = `Website`;
         
-        image.setAttribute('src', businessDirectory.imageurl);
-        image.setAttribute('alt', `${businessDirectory.name} Logo`);
+        image.setAttribute('src', business.imageurl);
+        image.setAttribute('alt', `${business.name} Logo`);
         image.setAttribute('loading', 'lazy');
         image.setAttribute('width', '340');
         image.setAttribute('height', '440');
 
+        card.appendChild(image);
         card.appendChild(name);
         card.appendChild(address);
         card.appendChild(phone);
         card.appendChild(website);
-        card.appendChild(image);
 
         cards.appendChild(card);
     })
