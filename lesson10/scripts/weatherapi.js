9c659fe0d8fa1613ab2f3ce261469927974b8622
braquelw <appaloosarider7@gmail.com>
2023-03-07 13:49:55 -0700

    added weather api

---
+++ lesson10/scripts/weatherapi.js
@@ -0,0 +1,31 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'http://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=3320c0ad0e45b9bff0bc10ad6dbe4d96';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  apiFetch();
+
+  function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  }
