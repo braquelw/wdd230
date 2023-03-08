3f35bf6ce884b04bba972d436ef69215246ee1c9
braquelw <appaloosarider7@gmail.com>
2023-03-08 13:03:23 -0700

    updated weather widget

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,17 +1,15 @@
-// Input
-const t = document.getElementById("temp").innerText;
-const s = document.getElementById("wspeed").innerText;
+function windChill(t, s) {
+  // Processing
+  if (t > 50 || s < 3)
+  {
+      chill = "N/A";
+  }
+  else
+  {
+      chill = Math.round(35.74 + (0.6215 * t) - (35.75 * (s ^ 0.16)) + (0.4275 * t * (s ^ 0.16)));
+      chill = chill  + "°F";
+  }
 
-// Processing
-if (t > 50 || s < 3)
-{
-    chill = "N/A";
+  // Output
+  document.getElementById('wchill').innerHTML = chill;
 }
-else
-{
-    chill = Math.round(35.74 + (0.6215 * t) - (35.75 * (s ^ 0.16)) + (0.4275 * t * (s ^ 0.16)));
-    chill = chill  + "°F";
-}
-
-// Output
-document.getElementById('wchill').innerHTML = chill;
